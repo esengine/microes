5783e96a931f4d9184e7aa8f92db376bd22d1308
esengine <359807859@qq.com>
2026-03-03 05:47:50 -0800

    fix: use world-space mouse coordinates in input-demo example

--- examples/input-demo/src/systems/mouse.ts
+++ examples/input-demo/src/systems/mouse.ts
@@ -1,17 +1,18 @@
 import {
     defineSystem, Query, Mut, Res, Input, Commands,
-    Transform, Sprite,
+    Transform, Sprite, UICameraInfo,
 } from 'esengine';
+import type { UICameraData } from 'esengine';
 import { MouseFollower, Trail } from '../components';
 
 const FOLLOW_SPEED = 8;
 
 export const mouseFollowSystem = defineSystem(
-    [Query(Mut(Transform), MouseFollower), Res(Input)],
-    (query, input) => {
-        const mouse = input.getMousePosition();
-        const worldX = mouse.x;
-        const worldY = mouse.y;
+    [Query(Mut(Transform), MouseFollower), Res(Input), Res(UICameraInfo)],
+    (query, input, camera: UICameraData) => {
+        if (!camera.valid) return;
+        const worldX = camera.worldMouseX;
+        const worldY = camera.worldMouseY;
 
         for (const [_entity, transform] of query) {
             transform.position.x += (worldX - transform.position.x) * FOLLOW_SPEED * 0.016;
@@ -22,12 +23,13 @@ export const mouseFollowSystem = defineSystem(
 );
 
 export const mouseClickSystem = defineSystem(
-    [Res(Input), Commands()],
-    (input, cmds) => {
+    [Res(Input), Res(UICameraInfo), Commands()],
+    (input, camera: UICameraData, cmds) => {
+        if (!camera.valid) return;
+
         if (input.isMouseButtonPressed(0)) {
-            const pos = input.getMousePosition();
             cmds.spawn()
-                .insert(Transform, { position: { x: pos.x, y: pos.y, z: 0 } })
+                .insert(Transform, { position: { x: camera.worldMouseX, y: camera.worldMouseY, z: 0 } })
                 .insert(Sprite, {
                     size: { x: 20, y: 20 },
                     color: { r: 1, g: 1, b: 0.3, a: 1 },
@@ -37,9 +39,8 @@ export const mouseClickSystem = defineSystem(
         }
 
         if (input.isMouseButtonPressed(2)) {
-            const pos = input.getMousePosition();
             cmds.spawn()
-                .insert(Transform, { position: { x: pos.x, y: pos.y, z: 0 } })
+                .insert(Transform, { position: { x: camera.worldMouseX, y: camera.worldMouseY, z: 0 } })
                 .insert(Sprite, {
                     size: { x: 16, y: 16 },
                     color: { r: 0.8, g: 0.3, b: 1, a: 1 },
